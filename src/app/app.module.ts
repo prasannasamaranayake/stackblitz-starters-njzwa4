7fab05666296ea72a40eee11470966ab1e555d0c
prasannasamaranayake <83085967+prasannasamaranayake@users.noreply.github.com>
2023-07-25 22:48:31 +0530

    1 file modified, 10 added, 10 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { AppComponent } from './app/app.component';
-import { SearchComponent } from './app/search/search.component';
-import { DisplayComponent } from './app/display/display.component';
+import { AppComponent } from './app.component';
+import { SearchComponent } from './search/search.component';
+import { DisplayComponent } from './display/display.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import 'zone.js/dist/zone';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule } from './app/app/app.module';
+import { AppModule } from './app/app.module';
 
 platformBrowserDynamic().bootstrapModule(AppModule);
